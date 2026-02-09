e863c37b00352e53b5dc150a6ef9293176d6aadb
Rajneshking <ahirwarrajnesh493@gmail.com>
2026-02-09 21:50:48 +0530

    dsa with js and java

---
+++ basics/day1.js
@@ -0,0 +1,49 @@
+// console.log(Math.floor(2344234/10));
+
+// // let i = 11
+// // i = i++ + ++i
+// // console.log(i)
+
+// let a=11, b=22
+//  let c = a+b+ a++ + b++ + ++a + ++b
+
+//  console.log(a)
+//  console.log(b)
+//  console.log(c) 
+
+// let i = true
+
+// console.log(++i)
+
+// console.log(Math.floor(10.5))
+// console.log(Math.ceil(10.1))
+// console.log(Math.round(10.8))
+// console.log(Math.trunc(18.8900))
+// console.log(Math.pow(2,4))
+// console.log(Math.sqrt(34))
+// console.log(Math.cbrt(34))
+// console.log(Math.abs(-34))
+// console.log(Math.max(-34,34,3,45,345))
+// console.log(Math.min(-34,34,3,45,345))
+// console.log(Math.random())
+// console.log(Math.trunc(Math.random()*9000)+1000)
+
+
+// let a = 89.0989; 
+
+// console.log(a.toFixed(2))
+
+// heroines formula
+// let a = 5 
+// let b = 4 
+// let c = 3
+
+// let s = (a+b+c)/2
+
+// console.log(s)
+
+// console.log(Math.sqrt(s * (s-a) * (s-b) * (s-c)))
+
+let r = 23
+
+console.log(Number((2*Math.PI*r).toFixed(2)))
---
+++ basics/day2.js
@@ -0,0 +1,78 @@
+// let ans = Number(prompt("Enter your age = "))
+
+// if(isNaN(ans)){
+//     console.log("wrong input ")
+// }
+// else if(ans>=18){
+//     console.log("your are eligible for vote ")
+// }
+// else {
+//      console.log("your are not eligible for vote ")
+// }
+
+// let amount = Number(prompt("enter your total amount = "))
+
+// if(amount>0 && amount<=5000){
+//     console.log(amount)
+// }
+// else if(amount>5000 && amount<=7000){
+//     console.log(amount - Math.floor((5*amount)/100))
+// }
+// else if(amount>7000 && amount<=9000){
+//     console.log(amount - Math.floor((10*amount)/100))
+// }
+// else if(amount>9000 ){
+//     console.log(amount - Math.floor((20*amount)/100))
+// }
+// else{
+//     console.log("wrong input ")
+// }
+
+// let amount = Number(prompt("enter your total amount = "))
+// console.log("you total amount is = "+amount)
+
+// if( amount >= 500){
+//     console.log("number of 500 ="+Math.floor(amount/500))
+//     amount = amount%500
+// }
+// if( amount >= 200){
+//     console.log("number of 200 ="+Math.floor(amount/200))
+//     amount = amount%200
+// }
+// if( amount >= 100){
+//     console.log("number of 100 ="+Math.floor(amount/100))
+//     amount = amount%100
+// }
+// if( amount >= 50){
+//     console.log("number of 50 ="+Math.floor(amount/50))
+//     amount = amount%50
+// }
+// if( amount >= 20){
+//     console.log("number of 20 ="+Math.floor(amount/20))
+//     amount = amount%20
+// }
+// if( amount >= 10){
+//     console.log("number of 10 ="+Math.floor(amount/10))
+//     amount = amount%10
+// }
+// if( amount >= 5){
+//     console.log("number of 5 ="+Math.floor(amount/5))
+//     amount = amount%5
+// }
+// if( amount >= 2){
+//     console.log("number of 2 ="+Math.floor(amount/2))
+//     amount = amount%2
+// }
+// if( amount >= 1){
+//     console.log("number of 1 ="+Math.floor(amount/1))
+//     amount = amount%1
+// }
+
+
+// //ternery operator 
+
+// let num = prompt("enter number for check its positive negative or zero ")
+
+// console.log(num>0?"posotive":num<0?"negative":"zero")
+
+
