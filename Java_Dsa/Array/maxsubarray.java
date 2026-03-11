a2077f71f1f3fe0cd1b909475a32db86831904eb
Rajneshking <ahirwarrajnesh493@gmail.com>
2026-03-11 14:04:43 +0530

    jj

---
+++ Java_Dsa/Array/maxsubarray.java
@@ -0,0 +1,18 @@
+package Java_Dsa.Array;
+
+public class maxsubarray {
+    public static void main(String[] args) {
+        int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int maxsum = Integer.MIN_VALUE;
+        for(int i =0 ; i<arr.length;i++){
+            int sum  = 0 ;
+            for(int j = i ; j<arr.length;j++){
+                sum = sum +arr[j];
+                if(sum>maxsum){
+                    maxsum = sum;
+                }
+            }
+        }
+        System.out.println("maximum subarray sum = "+ maxsum);
+    }
+}
